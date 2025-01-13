babf82fc751a50b11ffe162c6fbe9742c58e2ea9
Kaushik Vishwakarma <vkaushik13804@gmail.com>
2025-01-13 14:45:25 +0530

    everything working fine

--- components/ChatForum.js
+++ components/ChatForum.js
@@ -1,23 +1,54 @@
 'use client'
-import { Chat, useCreateChatClient } from 'stream-chat-react';
 
-// your Stream app information
-const apiKey = 'dz5f4d5kzrue';
+import { useState, useEffect } from 'react';
+import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import 'stream-chat-react/dist/css/v2/index.css';
+
+// Stream app configuration
+const apiKey = process.env.apiKeyi ||'dz5f4d5kzrue';
 const userId = 'lucky-bar-6';
 const userName = 'lucky';
 const userToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoibHVja3ktYmFyLTYiLCJleHAiOjE3MzY3NzU2MDF9.6QQy1zeMdRDKqPRxWcKQhByIWK5JfnuIukrf2yo8Kj8';
 
-export default  function ChatForum() {
-    const client = useCreateChatClient({
-        apiKey,
-        tokenOrProvider: userToken,
-        userData: { id: userId },
-      });
+const user = {
+  id: userId,
+  name: userName,
+  image: `https://getstream.io/random_png/?name=${userName}`,
+};
 
-      
+export default function ChatForum() {
+  const [channel, setChannel] = useState(null);
+  
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: user,
+  });
+
+  useEffect(() => {
+    if (!client) return;
     
+    const channel = client.channel('messaging', 'custom_channel_id', {
+      image: 'https://getstream.io/random_png/?name=react',
+      name: 'Talk about React',
+      members: [userId],
+    });
+    
+    setChannel(channel);
+  }, [client]);
 
-    if (!client) return <div>Setting up client & connection...</div>;
+  if (!client) return <div>Setting up client & connection...</div>;
 
-  return <Chat client={client}>Chat with client is ready!</Chat>;
-  }
+  return (
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+}
