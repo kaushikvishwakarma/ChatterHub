14819f75f873a13e0a6cc5fb95dc0cd70ada1dcd
Kaushik Vishwakarma <vkaushik13804@gmail.com>
2025-01-16 23:49:09 +0530

    All things done and working

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,16 +1,46 @@
 import { StreamChat } from "stream-chat"
 
+import { clerkClient } from '@clerk/nextjs/server';
+
+
 //ENter you api key and secret here which you will get from getstream app 
 const api_key = process.env.NEXT_PUBLIC_API_KEY;
 const api_secret =process.env. NEXT_PUBLIC_SECRET_KEY;
 
 // const user_id = "user_2rX78b1iKxR1KkMME6SkAgTdMf8";
+
 export async function POST(req) {
+  function capitalize(input) {
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  }  
   
   const serverClient = StreamChat.getInstance(api_key, api_secret);
   const user=await req.json();
   
-  // const token=serverClient.createToken(user.id)
-  console.log(user.data.id);
+  const token=serverClient.createToken(user.data.id)
+
+  console.log(token);
+  const client = await clerkClient();
+  await serverClient.upsertUser({id: user.data.id,role: 'admin'});
+
+  await client.users.updateUserMetadata(user.data.id, {
+    publicMetadata: {
+      token:token
+    },
+  })
+
+  // Giving  access to all users 
+  const slugs = ["python-new", "JS-new", "Cpp-new", "Blender-new", "UnrealEngine-new", "git-new"];
+  slugs.forEach(async(item)=>{
+    const channel = serverClient.channel('messaging', item, {
+      image: 'https://getstream.io/random_png/?name=react',
+      
+      name: capitalize(item)+' Discussion',
+      created_by_id:user.data.id,
+    });
+    await channel.create();
+    await channel.addMembers([user.data.id]);
+
+  })
   return Response.json({ message: 'Hello World' })
 }
--- components/ChatForum.js
+++ components/ChatForum.js
@@ -1,13 +1,61 @@
 'use client'
 
-import React from 'react'
-import { currentUser } from '@clerk/nextjs/server'
-const ChatForum = (clerkUser,slug) => {
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useCreateChatClient, Chat, Channel, ChannelHeader, MessageInput, MessageList, Thread, Window } from 'stream-chat-react';
+import 'stream-chat-react/dist/css/v2/index.css';
+
+function capitalize(input) {
+  
+  return input.charAt(0).toUpperCase() + input.slice(1);
+}
+
+
+const ChatForum = ({clerkUser, slug}) => {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const userId = clerkUser.id;
+  const userName = clerkUser.name;
+  const userToken = clerkUser.token;
+
+  const user = {
+    id: userId,
+    name: userName,
+    image: `https://getstream.io/random_png/?name=${userName}`,
+  };
+
+  const [channel, setChannel] = useState();
+  const client = useCreateChatClient({
+    apiKey,
+    tokenOrProvider: userToken,
+    userData: user,
+  });
+
+  useEffect(() => {
+    if (!client) return;
+
+    const channel = client.channel('messaging', slug, {
+      image: 'https://getstream.io/random_png/?name=react',
+      name: capitalize(slug)+' Discussion',
+    });
+
+    setChannel(channel);
+    
+  }, [client]);
+
+  if (!client) return <div>Setting up client & connection...</div>;
+
   return (
-    <div>
-      chat forum
-    </div>
-  )
+    <Chat client={client}>
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
 }
 
 export default ChatForum
